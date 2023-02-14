c0353fdbadbf61f54d6e67376227e48233b4ad5f
Emelin Bertoncello <88146420+emybertoncello@users.noreply.github.com>
2023-02-14 12:00:05 -0300

    Add files via upload

---
+++ AreaRetangulo/AreaRetangulo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+namespace AreaRetangulo
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Retangulo r = new Retangulo();
+
+            Console.WriteLine("Entre com a largura e altura do retangulo: ");
+            r.Largura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            r.Altura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("AREA = " + r.Area().ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("PERIMETRO = " + r.Perimetro().ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("DIAGONAL = " + r.Diagonal().ToString("F2",CultureInfo.InvariantCulture));
+        }
+    }
+}
